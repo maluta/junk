194871dc1cce56446a417fdc23a43a3f6391b433
Tiago Maluta <maluta@unifei.edu.br>
2010-01-08 16:38:02 -0200

    Added a little script to rip (to wav) CDROM content

---
+++ cdrip.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import subprocess
+import CDDB, DiscID
+
+cdrom = DiscID.open()
+disc_id = DiscID.disc_id(cdrom)
+
+(query_status, query_info) = CDDB.query(disc_id)
+(read_status, read_info) = CDDB.read(query_info['category'], query_info['disc_id'])
+x=0
+for i in range(disc_id[1]):
+	name = "%s.wav" % (read_info['TTITLE' + `i`])
+        name = name.replace("\xba",".")
+	name = name.replace("\"","'")
+	name = name.replace(",","")
+	if i<9:
+		mplayer_cmd="mplayer cdda://%d -ao pcm:file=\"0%d - %s\" " % (i+1,i+1,name)
+	else:
+		mplayer_cmd="mplayer cdda://%d -ao pcm:file=\"%d - %s\" " % (i+1,i+1,name)
+
+	print mplayer_cmd
+#	subprocess.call(mplayer_cmd,shell=True)
+
